731d79f885f42ce96d1a28e8df5a831a30d5e523
unknown <dushyantsirohi100@gmail.com>
2023-02-07 19:17:21 +0530

    first commit

---
+++ src/app/angular/angular.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { EnrollService } from '../Services/enroll.service';
+
+@Component({
+  selector: 'app-angular',
+  templateUrl: './angular.component.html',
+  styleUrls: ['./angular.component.css'],
+  //providers:[EnrollService]
+})
+export class AngularComponent {
+  title="angular";
+  constructor(private enrollService:EnrollService){
+
+  }
+  OnEnroll(){
+   
+   this.enrollService.OnEnrollClicked(this.title);
+  }
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AngularComponent } from './angular/angular.component';
+import { JavascriptComponent } from './javascript/javascript.component';
+import { EnrollService } from './Services/enroll.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AngularComponent,
+    JavascriptComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers:[EnrollService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
